261fb8f867e558b05746355d39976c69f90cf997
Absent1337 <107310248+Absent1337@users.noreply.github.com>
2024-05-30 23:02:37 +0200

    Changed title

--- htdocs/index.php
+++ htdocs/index.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Logowanie</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
